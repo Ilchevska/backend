8a02a0e8eeb7ec3586b390f44ce45127be7db089
Ilchevska <72986284+Ilchevska@users.noreply.github.com>
2021-01-20 10:04:10 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ server.set('view engine', 'ejs')
 var collection;
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
   });
 
