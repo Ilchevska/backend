a86d9106c82439c4b6320e4c21c0cf5dedfe85ae
Ilchevska <72986284+Ilchevska@users.noreply.github.com>
2021-01-20 10:06:19 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ server.get("/get/:id", async (request, response) => {
 })
 
 server.put("/update/:id", async (request, response) => {
+    res.header('Access-Control-Allow-Origin', '*');
     const data = req.body;
     var id = req.body;
     client.connect(function (err, db) {
