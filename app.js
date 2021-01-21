197329bfbea352c87cd27a15ef2c0e4d80d023da
Ilchevska <72986284+Ilchevska@users.noreply.github.com>
2021-01-21 13:00:52 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var collection;
 
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    req.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   }); 
 
