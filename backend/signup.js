cc85bd0e28d20458cb2cb9ae28d7b5b6cb00803e
Ilchevska <72986284+Ilchevska@users.noreply.github.com>
2021-01-26 12:41:46 +0000

    Revert "change"
    This reverts commit 887dd3b0659ac0c944576ee1f6600fbe28bde63c.

--- backend/signup.js
+++ backend/signup.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("load", function(){
     var submitBtn = document.getElementById("submit");
     submitBtn.addEventListener("click", submitForm);
@@ -15,81 +16,68 @@ window.addEventListener("load", function(){
     }
 
     var id; 
-    $(document).ready(function() {
-        $("#citID").autocomplete({
-            source: async function(request, response) {
-                let data = await fetch(`/search?citID=${request.term}`)
-                .then(results => results.json())
-                .then(results => results.map(result => {
-                    return { 
-                        label: result.citID, 
-                        id: result._id, 
-                        fName: result.firstName,
-                        lName: result.lastName,
-                        email: result.email,
-                        DOB: result.DOB,
-                        status: result.applicationStatus,
-                        number: result.phoneNumber
-                    };
-                }));
-                response(data);
-    
-            },
-            minLenght: 2,
-            select: async function(event, ui) {
-                id = ui.item.id;
-                let firstName = ui.item.fName;
-                $('#firstName').val(firstName)
-                let lastName = ui.item.lName;
-                $('#lastName').val(lastName);
-                let email = ui.item.email;
-                $('#email').val(email);
-                let number = ui.item.number;
-                $('#phone').val(number);
-                let status = ui.item.status;
-                $('#currentStatus').val(status)
-                let DOB = ui.item.DOB;
-                // $('#DOB').val(DOB)
-            }
-        });
-    });
-    
-    $(document).ready(function(){
-    
-        $('.updateEvent').on('click', editEvent);
-    
-    });
-    
-    function editEvent(){
-        var status = document.getElementById("status");
-        var update = status.options[status.selectedIndex].text;
-    
-        $.ajax({
-        url: '/update/' + id,
-        type:'PUT',
-        dataType: "json",
-        contentType: "application/json",
-        data: JSON.stringify({"applicationStatus": update}),
-        }).done(function(){
-    
-        }).fail(function(response){
-        console.log("Oops not working \n" );
-        });
+$(document).ready(function() {
+    $("#citID").autocomplete({
+        source: async function(request, response) {
+            let data = await fetch(`/search?citID=${request.term}`)
+            .then(results => results.json())
+            .then(results => results.map(result => {
+                return { 
+                    label: result.citID, 
+                    id: result._id, 
+                    fName: result.firstName,
+                    lName: result.lastName,
+                    email: result.email,
+                    DOB: result.DOB,
+                    status: result.applicationStatus,
+                    number: result.phoneNumber
+                };
+            }));
+            response(data);
 
-        swal({
-            position: 'top-end',
-            icon: 'success',
-            text: 'Application status was updated',
-            buttons: false,
-            timer: 1500
-          })
-    
-        setTimeout(function() {
-            window.open("index.html", "_self");
-        }, 2000)
-
+        },
+        minLenght: 2,
+        select: async function(event, ui) {
+            id = ui.item.id;
+            let firstName = ui.item.fName;
+            $('#firstName').val(firstName)
+            let lastName = ui.item.lName;
+            $('#lastName').val(lastName);
+            let email = ui.item.email;
+            $('#email').val(email);
+            let number = ui.item.number;
+            $('#phone').val(number);
+            let status = ui.item.status;
+            $('#currentStatus').val(status)
+            let DOB = ui.item.DOB;
+            // $('#DOB').val(DOB)
         }
+    });
+});
 
+$(document).ready(function(){
+$('.updateEvent').on('click', editEvent);
+});
+
+
+function editEvent(){
+var status = document.getElementById("status");
+var update = status.options[status.selectedIndex].text;
+
+$.ajax({
+url: '/update/' + id,
+type:'PUT',
+dataType: "json",
+contentType: "application/json",
+data: JSON.stringify({"applicationStatus": update}),
+}).done(function(response){
+    alert("Application status has been updated to: " + update)
+    window.open("index.html", "_self")
+}).fail(function(response){
+console.log("Oops not working \n" );
+});
+
+}
 });
 
 
