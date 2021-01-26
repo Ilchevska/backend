f9677886d7148ee3fd38db4da39dd1da1bf385cb
Ilchevska <72986284+Ilchevska@users.noreply.github.com>
2021-01-26 09:34:07 +0000

    launch

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const Cors = require("cors");
 const BodyParser = require("body-parser");
 const {request, response} = require("express");
+var path = require('path');
+
 
 const client = new MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@twilio.ex8eh.azure.mongodb.net/twilio?retryWrites=true&w=majority");
 const server = express();
@@ -14,13 +16,12 @@ server.set('view engine', 'ejs')
 
 var collection;
 
-server.use((req, res, next) => {
-    console.log(req.headers)
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header("Access-Control-Allow-Methods", "POST,PUT,GET");
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
-  }); 
+server.use(express.static(path.join(__dirname)));
+
+
+server.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + 'views/index.html'));
+});
 
 server.get("/search", async (request, response) =>  {
     try {
@@ -51,57 +52,26 @@ server.get("/get/:id", async (request, response) => {
     }
 })
 
-server.put("/update/:id", async (request, response) => {
-//   response.setheader['X-Requested-With'] = 'xmlhttprequest'
-//    request.header("X-Requested-With", "XMLHttpRequest");
-//    response.header("X-Requested-With", "XMLHttpRequest");
-
+server.put('/update/:id', async (request, response) => {
 
     try {
         let data = request.body;
-        var id = request.params.id;
-        console.log(id);
+        let id = request.params.id;
+
         client.connect(function (err, db) {
             if (err) throw err;
             collection.updateOne({"_id": ObjectID(id)}, { $set: data }, function (err, result) {
-                if (err) {
-                    console.log(id);
-                    throw err;
-                } else {
-                console.log("Items updated");
-                db.close(); 
-                }
-            })
+            db.close(); 
         })
-/*         client.connect(function (err, db) {
-            if (err) throw err;
-            collection("applicantDetails").updateOne({"_id": ObjectID(id)}, { $set: data }, function (err, result) {
-                console.log("Items updated");
-                db.close(); 
-        })*/
+    })
     }catch (e) {
 
     }
 })
 
-    // let result = await collection.findOne({ "_id": ObjectID(id)});
-    // response.send(result);
- 
-
-/* server.get("/applicant_details", async (request, response) => {
-        const db = client.db("twilio");
-        const collection = db.collection("applicantDetails");
-        collection.findOne({ "_id": ObjectID(request.params.id)})
-        if (details) {
-            var applicantName = details.firstName;
-            response.send(applicantName);
-        }
-}) */
-
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, async () => {
     try {
-        console.log(`Server is listening on port...`)
         await client.connect();
         collection = client.db("twilio").collection("applicantDetails");
     } catch (e) {
---
+++ backend/signup.js
@@ -0,0 +1,85 @@
+//alert("JavaScript file connected");
+
+window.addEventListener("load", function(){
+    var submitBtn = document.getElementById("submit");
+    submitBtn.addEventListener("click", submitForm);
+    
+    function submitForm(){
+                
+        if(citID.value === ""){           
+           citIDError.innerHTML = "Please enter an application ID";
+           citIDError.style.fontSize = "1em";
+           citIDError.style.color = "red";        
+        }
+        else {
+           result.style.display = "block"
+        // window.open("get_application.html", "_self")
+        }
+    }
+
+    var id; 
+$(document).ready(function() {
+    $("#citID").autocomplete({
+        source: async function(request, response) {
+            let data = await fetch(`/search?citID=${request.term}`)
+            .then(results => results.json())
+            .then(results => results.map(result => {
+                return { 
+                    label: result.citID, 
+                    id: result._id, 
+                    fName: result.firstName,
+                    lName: result.lastName,
+                    email: result.email,
+                    DOB: result.DOB,
+                    status: result.applicationStatus,
+                    number: result.phoneNumber
+                };
+            }));
+            response(data);
+
+        },
+        minLenght: 2,
+        select: async function(event, ui) {
+            id = ui.item.id;
+            let firstName = ui.item.fName;
+            $('#firstName').val(firstName)
+            let lastName = ui.item.lName;
+            $('#lastName').val(lastName);
+            let email = ui.item.email;
+            $('#email').val(email);
+            let number = ui.item.number;
+            $('#phone').val(number);
+            let status = ui.item.status;
+            $('#currentStatus').val(status)
+            let DOB = ui.item.DOB;
+            // $('#DOB').val(DOB)
+        }
+    });
+});
+
+$(document).ready(function(){
+$('.updateEvent').on('click', editEvent);
+});
+
+
+function editEvent(){
+var status = document.getElementById("status");
+var update = status.options[status.selectedIndex].text;
+
+$.ajax({
+url: '/update/' + id,
+type:'PUT',
+dataType: "json",
+contentType: "application/json",
+data: JSON.stringify({"applicationStatus": update}),
+}).done(function(response){
+
+}).fail(function(response){
+console.log("Oops not working \n" );
+});
+
+}
+});
+
+
+
